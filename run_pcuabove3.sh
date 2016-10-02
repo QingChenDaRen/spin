4227e3985e1b91886fc5a4ab69da65bbe4733eab
Charith <charith.peris@gmail.com>
2016-10-01 22:42:56 -0400

    Preliminary spin code
    These files consist of the preliminary code built to do the spin fitting on xspec`

---
+++ run_pcuabove3.sh
@@ -0,0 +1,39 @@
+N=1
+
+while read line
+do 
+
+obsname=`echo ${line} | awk '{print $1}'`
+String=`echo ${line} | awk '{print $2}'`
+StringLength=`echo ${line} | awk '{print $2}' | awk '{print length}'`
+
+#echo $StringLength
+
+if [ "$StringLength" -gt 2 ]
+then
+	echo "$String"
+
+#	for (( i=0; i<${#String}; i++ )); do
+       for (( i=0; i<${StringLength}; i++ )); do
+  		echo "${String:$i:1}"
+  		pcunum[${i}]="${String:$i:1}" 
+	done
+
+xspec<<EOF
+
+source mymodelspin_multipcu.tcl
+myfit $N ${obsname} [list ${pcunum[*]}]
+
+EOF
+
+unset pcunum
+
+N=$((N+1))
+
+else
+	echo "Short"
+fi
+
+#done < numpcu_obs_hard.ls
+#done < numpcu_obs_hard_relxill.ls
+done < test.ls
