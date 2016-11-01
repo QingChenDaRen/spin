31931d86f48426d7f5c5196430974d42cfebf756
Charith <charith.peris@gmail.com>
2016-10-31 21:51:58 -0400

    Remove duplicated directory

--- spin_code_matlab/bin/csp_wmean.m
+++
@@ -1,7 +0,0 @@
-function [wm wmerr] = csp_wmean(x, xerr)
-%This function calculates the weighted mean and its error given values and errors
-%Corrects for variance/dispersion
-
-wm = sum(x./(xerr.^2))/sum(1./(xerr.^2));
-wmerr = sqrt((1/sum(1./(xerr.^2)))*(1/(length(x)-1))*sum(((x - wm).^2)./(xerr.^2)));
-end
