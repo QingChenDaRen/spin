28b53a53833689b3c1055655fdbc48ae9588fd0f
Charith Peris <cperis@ciri.mit.edu>
2017-02-03 20:27:56 -0500

    modified test.tcl

--- test.tcl
+++ test.tcl
@@ -12,10 +12,12 @@ cd ..
 # Set model number
 set mod m98
 # Define new models in xspec.init
-#load /Users/charith/Documents/xrayspectralanalysis/software/xspec_local_models/simplcut/libsimplcut.dylib
-load /Users/charith/Documents/xrayspectralanalysis/software/xspec_local_models/simplcomp/libsimplcomp.dylib
-load /Users/charith/Documents/xrayspectralanalysis/software/xspec_local_models/relxill/librelxill.dylib
-load ~/Documents/xrayspectralanalysis/software/xspec_local_models/simplcutx/libjscutx.dylib
+#load /ciri/astro/xspec_local_libs/XSPEC_DIR64/XKERRBB/libjsxkbb.dylib
+load /ciri/astro/xspec_local_libs/XSPEC_DIR64/public_SIMPLCUT/libjscutx.so
+#load /ciri/astro/xspec_local_libs/XSPEC_DIR64/RELXILL_v04a_FAST/librelxill.dylib 
+#load /ciri/astro/xspec_local_libs/XSPEC_DIR64/RELXILL_v04g/librelxill.so
+load /ciri/astro/xspec_local_libs/XSPEC_DIR64/relxill-v0.4j/librelxill.so 
+
 
 # Set ALPHA value when using kerrbb for disk
 set alpha 0.05
@@ -45,7 +47,7 @@ set obsname [lindex $y 0].[lindex $y 1].[lindex $y 2]
 
 # Set up the model.
   model none
-  model TBabs*simplcutx*(ezdiskbb + relxill)*gabs*smedge & /*
+  model TBabs*simplcutx*(kerrbb + relxill)*gabs*smedge & /*
 
 # Set up parameters
   newpar 1 5.0 0.0
@@ -53,31 +55,49 @@ set obsname [lindex $y 0].[lindex $y 1].[lindex $y 2]
   newpar 3 0.05 0.005 0.0 0.0 1.0 1.0
   newpar 4 -1.0 0.0
   newpar 5 100.0 0.01 10.0 10.0 100000 100000
-  newpar 6 1.5 0.01 0.5 0.5 4.0 4.0
-  newpar 7 300 0.01 0.00000001 0.00000001 10000 10000
-  newpar 8=3
-  newpar 9=8
+  newpar 6 0.0 -0.01
+  newpar 7 0.7 -0.01
+  newpar 8 60.0 -0.01
+  newpar 9 12.6 -0.01
   newpar 10 15.0 -0.01
-  newpar 11 0.998 -0.01
-  newpar 12 60.0 -0.01
-  newpar 13 -1.23 -0.01
-  newpar 17 3.1 -0.01
-  newpar 18 1.0 -0.01
-  newpar 19=abs(p6)
-  newpar 20=-4
-  newpar 21 1.0 -0.01
-  newpar 22 1.0 -0.01
-  newpar 23 7.0 0.01 6.3 6.3 8.5 8.5
-  newpar 24 0.5 -0.01
-  newpar 25 1.0 0.01
-  newpar 26 8.0 0.01 6.9 6.9 9.0 9.0
-  newpar 29 7.0 -0.01
+  newpar 11 8.6 -0.01
+  newpar 14 1.0 -0.01
+  newpar 15 1.0 -0.01
 
-  freeze 1 4 10 11 12 13 17 18 21 22 24 29
 
+  newpar 16 3.0 -0.01
+  newpar 17=16
+  newpar 18 15.0 -0.01
+  newpar 19=7
+  newpar 20=8
+  newpar 21 -1.23 -0.01
+  newpar 22 400.0 -0.01
+  newpar 23 0.0 -0.01
+  newpar 24=2
+  newpar 25 3.1 -0.01
+  newpar 26 1.0 -0.01
+  newpar 27=5*0.3     
+# Check this Ecut kTe relationship 
+  newpar 28=abs(p6)
+  newpar 29 1.0 -0.01
+
+  newpar 30 7.0 0.01 6.3 6.3 8.5 8.5
+  newpar 31 0.5 -0.01
+  newpar 32 1.0 0.01
+
+  newpar 33 8.0 0.01 6.9 6.9 9.0 9.0
+  newpar 36 7.0 -0.01
+
+  freeze 1 4 6 7 8 9 10 11 14 15 16 18 21 22 23 25 26 29 31 36
+
+  fit 10
 # Set systematics
 # systematic 0.01
+  thaw 29
 
+  fit 10
+
+  thaw 7
 # Run the fit
   fit 1000
 
